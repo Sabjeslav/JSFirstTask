77df46c0eba8f3e96a44b012a96f5a1b34092fce
Sabjeslav <s4tenko@gmail.com>
2021-03-24 09:38:19 +0200

    JSDoc function description added

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,12 @@ const secondNum = +prompt("Enter second num:");
 const num = +prompt("Enter number to check:");
 
 //task 1
+/**
+ * Finds maximal value of two numbers
+ * @param {number} val1 - first number to compare
+ * @param {number} val2 - second number to compare
+ * @returns {number} - bigger number of 2 arguments
+ */
 const maxValue = function (val1, val2) {
   if (isNaN(val1 - val2)) return null;
   if (val1 > val2) return val1;
@@ -10,6 +16,12 @@ const maxValue = function (val1, val2) {
 };
 
 //task 2
+/**
+ * Finds minimal value of two numbers
+ * @param {number} val1 - first number to compare
+ * @param {number} val2 - second number to compare
+ * @returns {number} - less number of 2 arguments
+ */
 const minValue = function (val1, val2) {
   if (isNaN(val1 - val2)) return null;
   if (val1 < val2) return val1;
@@ -17,14 +29,22 @@ const minValue = function (val1, val2) {
 };
 
 //task 3
+/**
+ * Checks if number is even
+ * @param {number} number - number to check
+ * @returns {boolean} - true if number is even, false if number is odd
+ */
 const checkParity = function (number) {
   return (number % 2 === 0);
 };
 
+/**
+ * Debug function for writing results into console
+ */
 function checkAnswers() {
   console.log("Max value is: ", maxValue(firstNum, secondNum));
   console.log("Min value is: ", minValue(firstNum, secondNum));
-  if (checkParity(num)) console.log(`${num} is even`);
+  if (checkParity(num)) console.log(`${num} is even`)
   else console.log(`${num} is odd`);
 }
 
@@ -34,6 +54,13 @@ const A = +prompt("Enter a:");
 const B = +prompt("Enter b:");
 const C = +prompt("Enter c:");
 
+/**
+ * Quadratic equation solve function
+ * @param {number} a - A coefficient of equation
+ * @param {number} b - B coefficient of equation
+ * @param {number} c - C coefficient of equation
+ * @returns {null | [number, number]} - null is discriminant === 0, array for x1, x2
+ */
 const quadratic = function (a, b, c) {
   const D = b ** 2 - 4 * a * c;
   console.log("Discriminant: ", D);
